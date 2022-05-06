57de658bfe038a46aa869dcce7999c28d615223f
Arthur Sandim <53281893+arthursandim@users.noreply.github.com>
2022-05-06 11:28:22 -0300

    .

---
+++ lista-6/02-fuso-horário_beecrowd-2057.py
@@ -0,0 +1,16 @@
+def calculoFuso (saida, tempo, fuso):
+    
+    chegada = saida + tempo + fuso
+    
+    if (chegada == 24):
+        print ('0')
+    elif (chegada > 24):
+        print (chegada - 24)
+    elif (chegada < 0):
+        print (24 + chegada)
+    else:
+        print (chegada)
+        
+saida, tempo, fuso = [int(x) for x in input().split()]
+
+calculoFuso (saida, tempo, fuso)
